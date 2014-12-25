e52be7e43838e5d5710ac5f53ff5b1c10549d000
jarvis-mark42 <rachna.singhal@yahoo.in>
2014-12-25 17:19:32 +0530

    auto comment on bday post on your timeline

---
+++ bday.py
@@ -0,0 +1,28 @@
+import sys
+import requests
+access_token=""
+timestamp=""
+li=[]
+def url_create():
+        url="https://graph.facebook.com/me/feed?since=%s&access_token=%s" % (timestamp,access_token)
+        return url
+ 
+def get_post(url):
+        req_obj=requests.get(url)
+        datas=req_obj.json()
+        return datas
+ 
+def get_id(datas):
+        for data in datas['data']:
+                li.append(str(data['id']))
+        return li
+ 
+def response(li):
+        for i in li:
+                url="https://graph.facebook.com/%s/comments?&access_token=%s" % (i,access_token)
+                requests.post(url,{"message" :"thanks"})
+if __name__ == "__main__":
+        url=url_create()
+        datas=get_post(url)
+        lis=get_id(datas)
+        response(lis)
